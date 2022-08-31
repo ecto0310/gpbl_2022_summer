4cc23de24a26e8fad3b15b451484c15da331b835
ecto0310 <ecto0310@gmail.com>
2022-08-31 10:48:46 +0700

    [add] chat view

--- src/c_meet/views/group.py
+++ src/c_meet/views/group.py
@@ -1,6 +1,7 @@
 from operator import truediv
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import (current_user, login_required)
+from c_meet.models.group_chat import Group_Chat
 from c_meet.models.group_user import Group_User
 from c_meet.models.hobbies import Hobby
 from c_meet.models.users import User
@@ -46,3 +47,26 @@ def complete_group(uuid):
     group_user.completed = True
     Group_User.update(group_user)
     return redirect(url_for('group.show_group', uuid = uuid))
+
+@group.route('/<uuid>/chat')
+@login_required
+def show_chat(uuid):
+    group = Group.query.filter(Group.id == uuid).first()
+    group_view = {}
+    group_view["uuid"]= uuid
+    group_view["date"]= group.date.date()
+    group_view["completed"]= Group_User.query.filter(Group_User.user_id == current_user.id, Group_User.group_id == group.id).first().completed
+    group_view["hobby"]= Hobby.query.filter(Hobby.id == group.hobby_id).first().type
+    group_view["place"]= group.place
+    chat_logs = []
+    logs = Group_Chat.query.filter(Group_Chat.group_id == group.id).all()
+    for log in logs:
+        chat = {}
+        user = User.search_id(log.user_id)
+        chat["me"] = True if  current_user.id  == log.user_id else False
+        chat["user_icon"] = user.icon
+        chat["user_name"] = user.name
+        chat["content"] = log.content
+        chat["date"] = log.created_at
+        chat_logs.append(chat)
+    return render_template("group/chat.html", group = group_view,  chat_logs = chat_logs )
